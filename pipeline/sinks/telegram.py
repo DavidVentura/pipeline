20b3f3afe4bb657c42d55e75b0ecf8a48e971425
david <davidventura27@gmail.com>
2018-07-24 23:09:10 +0200

    do not use markdown for now - it breaks when using invalid markdown

--- pipeline/sinks/telegram.py
+++ pipeline/sinks/telegram.py
@@ -6,6 +6,12 @@ class Telegram(SinkMixin):
     def put_value(self, data):
         url = "https://api.telegram.org/bot{bot_id}/sendMessage".format(**self.config)
         try:
-            r = requests.get(url, params={'chat_id': self.config['chat_id'], 'text': data, 'parse_mode': 'markdown'})
+            #r = requests.get(url, params={'chat_id': self.config['chat_id'], 'text': data, 'parse_mode': 'markdown'})
+            r = requests.get(url, params={'chat_id': self.config['chat_id'], 'text': data})
+            if r.status_code != 200:
+                print("Got %s" % data)
+                print(self.config)
+                print(url)
+                print(r.text)
         except Exception as e:
             print(e)
