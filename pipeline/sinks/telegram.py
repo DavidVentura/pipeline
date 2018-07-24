4fa73440c27c14322ba16d20bb6fb14d4d18d9c7
david <davidventura27@gmail.com>
2018-07-24 19:17:32 +0200

    parse messages in markdown by default

--- pipeline/sinks/telegram.py
+++ pipeline/sinks/telegram.py
@@ -5,4 +5,4 @@ from pipeline.mixins.mixins import SinkMixin
 class Telegram(SinkMixin):
     def put_value(self, data):
         url = "https://api.telegram.org/bot{bot_id}/sendMessage".format(**self.config)
-        requests.get(url, params={'chat_id': self.config['chat_id'], 'text': data})
+        requests.get(url, params={'chat_id': self.config['chat_id'], 'text': data, 'parse_mode': 'markdown'})
