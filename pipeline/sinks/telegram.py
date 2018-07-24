871c7cd5e464f0bc63471789b0167f7c2ba3d909
david <davidventura27@gmail.com>
2018-07-24 22:54:15 +0200

    add try/catch around posting to telegram

--- pipeline/sinks/telegram.py
+++ pipeline/sinks/telegram.py
@@ -5,4 +5,7 @@ from pipeline.mixins.mixins import SinkMixin
 class Telegram(SinkMixin):
     def put_value(self, data):
         url = "https://api.telegram.org/bot{bot_id}/sendMessage".format(**self.config)
-        requests.get(url, params={'chat_id': self.config['chat_id'], 'text': data, 'parse_mode': 'markdown'})
+        try:
+            r = requests.get(url, params={'chat_id': self.config['chat_id'], 'text': data, 'parse_mode': 'markdown'})
+        except Exception as e:
+            print(e)
