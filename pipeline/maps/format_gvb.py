ad7f5197b929f9fa9f34197bb99c1d97a3643381
david <davidventura27@gmail.com>
2018-07-24 18:56:49 +0200

    add gvb files

---
+++ pipeline/maps/format_gvb.py
@@ -0,0 +1,22 @@
+import datetime
+from pipeline.mixins.mixins import MapMixin
+
+def parse_date(date):
+    return datetime.datetime.strptime(date.split("+")[0], "%Y-%m-%dT%H:%M:%S")
+
+def print_diversion(diversion):
+    ret = ""
+    start = parse_date(diversion['startsAt']).strftime('%d/%m %H:%M')
+    end = parse_date(diversion['endsAt']).strftime('%d/%m %H:%M')
+    ret += "Start: %s -- End: %s\n" % (start, end)
+    ret += "*%s*\n" % diversion['title']
+    ret += "*Cause*: %s\n" % diversion['cause']
+    ret += "*Effect*: %s\n" % diversion['effect']
+    ret += "\n"
+    ret += diversion['description']
+    return ret
+
+
+class FormatGvb(MapMixin):
+    def process(self, data):
+        return [print_diversion(d) for d in data]
