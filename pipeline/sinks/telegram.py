f8f156e905c40ac901de08567b56f52c63a89047
david <davidventura27@gmail.com>
2018-07-25 20:37:11 +0200

    allow parse_mode to be configurable

--- pipeline/sinks/telegram.py
+++ pipeline/sinks/telegram.py
@@ -4,10 +4,20 @@ from pipeline.mixins.mixins import SinkMixin
 
 class Telegram(SinkMixin):
     def put_value(self, data):
+        parse_mode = 'markdown'
+        if 'parse_mode' in self.config:
+            parse_mode = self.config['parse_mode']
+
+        params = {'chat_id': self.config['chat_id'],
+                  'text': data,
+                  'parse_mode': parse_mode}
+
+        if parse_mode == 'text':
+            del params['parse_mode']
+
         url = "https://api.telegram.org/bot{bot_id}/sendMessage".format(**self.config)
         try:
-            #r = requests.get(url, params={'chat_id': self.config['chat_id'], 'text': data, 'parse_mode': 'markdown'})
-            r = requests.get(url, params={'chat_id': self.config['chat_id'], 'text': data})
+            r = requests.get(url, params=params)
             if r.status_code != 200:
                 print("Got %s" % data)
                 print(self.config)
