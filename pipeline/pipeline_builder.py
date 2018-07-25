33cdb0dd59c4d102ed201f9b5487e4de3bde0897
david <davidventura27@gmail.com>
2018-07-25 20:36:50 +0200

    add support for overriding a plugin's config on its pipeline

--- pipeline/pipeline_builder.py
+++ pipeline/pipeline_builder.py
@@ -6,12 +6,23 @@ def build(name):
     config = parser._sections
     pconfig = config['pipeline.%s' % name]
     pipeline = [plugin.strip() for plugin in pconfig['pipeline'].split('|')]
+    del pconfig['pipeline']
+
     ret = []
     for plugin in pipeline:
         plugin_conf = None
         plugin_name = plugin.split('.')[-1]
         config_section = 'plugin.%s' % plugin_name
+
         if config_section in config:
             plugin_conf = dict(config[config_section])
+
+        # pipeline overrides of plugin conf
+        for key, value in pconfig.items():
+            target_plugin_name, target_plugin_key = key.split(".")
+            if target_plugin_name == plugin_name.lower():
+                plugin_conf[target_plugin_key] = value
+
         ret.append({'plugin': plugin, 'config': plugin_conf})
+
     return ret
