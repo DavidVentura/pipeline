b4c4a84e4e7f702a05734e120d4f3d536d30bbda
david <davidventura27@gmail.com>
2018-07-25 20:46:56 +0200

    add an example of state on dummy

--- pipeline/filters/dummy.py
+++ pipeline/filters/dummy.py
@@ -1,5 +1,15 @@
 from pipeline.mixins.mixins import FilterMixin
 
 class Dummy(FilterMixin):
+    count = 0
     def filter(self, data):
-        return 'World' in data
+        self.state['count'] += 1
+        return 'Hello%d' % self.state['count'] in data
+
+    def get_state(self):
+        return self.state
+
+    def set_state(self, state):
+        if 'count' not in state:
+            state['count'] = 0
+        self.state = state
--- pipeline/sources/dummy.py
+++ pipeline/sources/dummy.py
@@ -3,4 +3,8 @@ from pipeline.mixins.mixins import SourceMixin
 class Dummy(SourceMixin):
     def get_value(self):
         yield 'Hello'
+        yield 'Hello2'
+        yield 'Hello'
+        yield 'Hello4'
+        yield 'Hello'
         yield 'World'
